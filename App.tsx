8ed9c29c91f1316ede0d9e371e49900b9fbd6254
Ayelen Guini <ayelen.guini@gmail.com>
2021-01-06 20:43:11 -0300

    First commit!

---
+++ App.tsx
@@ -0,0 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import { Colors } from './src/theme/Colors';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        
+              <Text style={styles.sectionTitle}>HELLO WORLD!</Text>
+              <Text style={styles.subtitle}>This will soon be a Minesweeper game :)</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.PINK,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.MUSTARD,
+  },
+
+  subtitle: {
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.LIGHT_BLUE,
+  }
+});
+
+export default App;
---
+++ src/theme/Colors.js
@@ -0,0 +1,12 @@
+/*
+ * Enum
+ */
+
+export const Colors = Object.freeze({
+  //  UI PRIMARY COLORS
+  MUSTARD: '#ffbc42',
+  PINK: '#d81159',
+  PURPLE: '#8f2d56',
+  MUSK_GREEN: '#218380',
+  LIGHT_BLUE: '#73d2de',
+});
